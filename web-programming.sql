ff672422602892cbfd29499d4508b3494047c754
Your Name <nguyenducanhtuan0602@gmail.com>
2024-04-22 14:27:40 +0700

    change .sql and add vendor

--- web-programming.sql
+++ web-programming.sql
@@ -20,7 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `web-programming`
 --
-
+CREATE DATABASE IF NOT EXISTS `web-programming` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `web-programming`;
 DELIMITER $$
 --
 -- Procedures
